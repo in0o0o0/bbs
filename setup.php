54b16f7376f713ea4ee83f923f1af9235cd272ed
inoue <vyma5xkhg8het@gmail.com>
2018-06-02 06:04:26 +0900

    [fix] テーブル名をthread_listに修正

--- setup.php
+++ setup.php
@@ -6,7 +6,7 @@ try {
   $dbh->query("use bbs");
 
   $user_data__sql = "CREATE TABLE user_data(id CHAR(10),nickname CHAR(16),password CHAR(128),registration_date CHAR(11),gender INT,age INT, self_introduction text,web_url text,img_url text)";
-  $thread_list_sql = "CREATE TABLE auth_thread_list (thread_title text,registration_time INT ,last_modified_time INT)";
+  $thread_list_sql = "CREATE TABLE thread_list (thread_title text,registration_time INT ,last_modified_time INT)";
 
   $dbh->exec($user_data__sql);
   $dbh->exec($thread_list_sql);
